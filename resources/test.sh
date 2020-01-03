6eebee78697c636abfb80316a32f6f4be9e6ccec
jnovotny <jnovotny@student.hive.fi>
2020-01-03 17:18:16 +0200

    added resources

---
+++ resources/test.sh
@@ -0,0 +1,28 @@
+# abanlin.filler		grati.filler		lcharvol.filler		pbondoer.filler
+# carli.filler		hcao.filler		mikim.filler		superjeannot.filler
+# champely.filler		ikozlov.filler		mtan.filler		vgladush.filler
+
+# rm -f logs.txt jn_log.txt
+# ./filler_vm -f maps/map02 -p1 players/mtan.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map02 -p1 players/ikozlov.filler -p2 players/jnovotny.filler
+
+# ./filler_vm -f maps/map00 -p1 players/abanlin.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map00 -p1 players/carli.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map00 -p1 players/champely.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map00 -p1 players/grati.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map00 -p1 players/hcao.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map00 -p1 players/superjeannot.filler -p2 players/jnovotny.filler
+
+# ./filler_vm -f maps/map01 -p1 players/abanlin.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map01 -p1 players/carli.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map01 -p1 players/champely.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map01 -p1 players/grati.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map01 -p1 players/hcao.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map01 -p1 players/superjeannot.filler -p2 players/jnovotny.filler
+
+# ./filler_vm -f maps/map02 -p1 players/abanlin.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map02 -p1 players/carli.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map02 -p1 players/champely.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map02 -p1 players/grati.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map02 -p1 players/hcao.filler -p2 players/jnovotny.filler
+# ./filler_vm -f maps/map02 -p1 players/superjeannot.filler -p2 players/jnovotny.filler
